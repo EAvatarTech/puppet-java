431d0fb9cdad323344b223c07eaeef9f6a7b6f56
Sam Kuo <sam@eavatar.com>
2013-10-30 21:45:09 +0800

    the code only works for Ubuntu systems.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ puppet-java
 ===========
 
 Puppet module for setting up Oracle JDK on Ubuntu systems.
+
+This module depends on following modules:
+
+* Apt
+* stdlib
+
---
+++ java/manifests/init.pp
@@ -0,0 +1,36 @@
+class java::java($version='present') {
+
+  exec { 'enforce_apt_update':
+    command => 'apt-get update',
+    path => '/usr/bin'
+  }
+  
+  
+  apt::ppa { 'ppa:webupd8team/java': 
+  }
+  
+
+  exec {
+    'set-licence-selected':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+ 
+    'set-licence-seen':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+  
+  }
+
+  package { 'oracle-java7-installer':
+    ensure => "${version}",
+    require => Apt::Ppa['ppa:webupd8team/java'], 
+  }
+
+  file { '/etc/profile.d/java.sh':
+    ensure => present,
+    source => "puppet:///modules/java/java.sh",
+    owner => "root",
+    group => "root",
+    mode => 644,
+    require => Package['oracle-java7-installer'],
+  }
+
+}
